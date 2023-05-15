0b2a58165a63e5ed445646407a2d536570262242
Jean <engjeanr@gmail.com>
2023-05-15 20:59:06 -0300

    Injetando um vendedor para testar se as configurações estão funcionando corretamente.

--- src/main/java/github/jeanalvesr/config/LocalConfig.java
+++ src/main/java/github/jeanalvesr/config/LocalConfig.java
@@ -1,15 +1,24 @@
 package github.jeanalvesr.config;
 
+import github.jeanalvesr.domain.entity.Vendedor;
+import github.jeanalvesr.domain.repository.Vendedores;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
 
 @Configuration
 @Profile("local")
 public class LocalConfig {
 
+    @Autowired
+    private Vendedores vendedores;
+
     @Bean
     public void startDB(){
-
+        Vendedor vendedor = new Vendedor(1, "Jean Alves", null);
+        vendedores.save(vendedor);
     }
 }
