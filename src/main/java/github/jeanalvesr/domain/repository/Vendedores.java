7e5b40e03aeb9ce0b359de00b3fa2270ff394720
Jean <engjeanr@gmail.com>
2023-05-15 19:55:34 -0300

    Add query personalizada para buscar vendedores dado a data inicial e final. Os vendedores retornados contém as vendas embutidas devido ao mapeamento na classe.

--- src/main/java/github/jeanalvesr/domain/repository/Vendedores.java
+++ src/main/java/github/jeanalvesr/domain/repository/Vendedores.java
@@ -2,6 +2,15 @@ package github.jeanalvesr.domain.repository;
 
 import github.jeanalvesr.domain.entity.Vendedor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface Vendedores extends JpaRepository<Vendedor, Integer> {
+
+    @Query("SELECT DISTINCT v FROM Vendedor v JOIN FETCH v.vendas vd WHERE vd.data >= :dataIni AND vd.data <= :dataFinal")
+    List<Vendedor> buscarVendedoresComVendasEntreDatas(@Param("dataIni") LocalDate dataIni, @Param("dataFinal") LocalDate dataFinal);
+
 }
